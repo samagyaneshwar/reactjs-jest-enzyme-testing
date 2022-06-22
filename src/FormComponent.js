d8a53e1ed5a5ebfc125737df7fae615e70c70f1b
Sama Gyaneshwar <sama.gyaneshwar@sourcefuse.com>
2022-06-22 17:15:58 +0530

    feat: add form component

---
+++ src/FormComponent.js
@@ -0,0 +1,9 @@
+import { Container } from "react-bootstrap";
+
+export default function FormComponent() {
+    return (
+        <Container className="mt-4">
+            hello
+        </Container>
+    )
+}
